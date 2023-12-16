89c71a1f553d0762f28400b07c719e00f3128b03
MikaParssinen <mika.77@live.se>
2023-12-16 12:40:35 +0100

    QR Scanning now WORKS
    Bugs: Crashes the app after some minuts maybe memory leak?

--- SmartRoomAR/Assets/Scripts/AROverlay/QRCodeDetector.cs
+++ SmartRoomAR/Assets/Scripts/AROverlay/QRCodeDetector.cs
@@ -5,79 +5,105 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using System;
 using Unity.Collections.LowLevel.Unsafe;
-
-
+using Unity.Collections;
 
 public class QRCodeDetector : MonoBehaviour
 {
+    [SerializeField]
     private ARCameraManager m_CameraManager;
+    [SerializeField]
+    private string _lastResult;
 
-    void Start()
+    private Texture2D _cameraImageTexture;
+
+    private IBarcodeReader _barcodeReader = new BarcodeReader
     {
-
-        // Get the ARCameraManager component
-        if (m_CameraManager == null)
-            m_CameraManager = GetComponent<ARCameraManager>();
-
-        if (m_CameraManager == null)
+        AutoRotate = false,
+        Options = new ZXing.Common.DecodingOptions
         {
-            Debug.LogError("ARCameraManager not found. Make sure the script is attached to a GameObject with ARCameraManager component.");
+            TryHarder = false
         }
-        else
-        {
-            Debug.Log("Found an ARCameraManager");
-        }
+    };
+
+    private Result _result;
 
 
 
+    private void OnEnable()
+    {
+        m_CameraManager.frameReceived += OnCameraFrameReceived;
+    }
+    private void OnDisable()
+    {
+        m_CameraManager.frameReceived -= OnCameraFrameReceived;
     }
 
-    void Update()
+
+
+    private void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
     {
-        // Acquire an XRCpuImage
-        if (!m_CameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
-            return;
-
-        // Set up our conversion params
-        var conversionParams = new XRCpuImage.ConversionParams
         {
-            // Convert the entire image
-            inputRect = new RectInt(0, 0, image.width, image.height),
+            // Acquire an XRCpuImage
+            if (!m_CameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
+                return;
 
-            // Output at full resolution
-            outputDimensions = new Vector2Int(image.width, image.height),
-
-            // Convert to RGBA format
-            outputFormat = TextureFormat.RGBA32,
-
-            // Flip across the vertical axis (mirror image)
-            transformation = XRCpuImage.Transformation.MirrorY
-        };
-
-        // Create a Texture2D to store the converted image
-        var texture = new Texture2D(image.width, image.height, TextureFormat.RGBA32, false);
-
-        // Texture2D allows us write directly to the raw texture data as an optimization
-        var rawTextureData = texture.GetRawTextureData<byte>();
-        try
-        {
-            unsafe
+            // Set up our conversion params
+            var conversionParams = new XRCpuImage.ConversionParams
             {
-                // Synchronously convert to the desired TextureFormat
-                image.Convert(
-                    conversionParams,
-                    new IntPtr(rawTextureData.GetUnsafePtr()),
-                    rawTextureData.Length);
-            }
-        }
-        finally
-        {
-            Debug.Log("Now we destroy!");
-            // Dispose the XRCpuImage after we're finished to prevent any memory leaks
-            image.Dispose();
-        }
+                // Convert the entire image
+                inputRect = new RectInt(0, 0, image.width, image.height),
 
-        // Apply the converted pixel data to our texture
-        texture.Apply();
+                // Output at full resolution
+                outputDimensions = new Vector2Int(image.width, image.height),
+
+                // Convert to RGBA format
+                outputFormat = TextureFormat.RGBA32,
+
+                // Flip across the vertical axis (mirror image)
+                transformation = XRCpuImage.Transformation.MirrorY
+            };
+
+            // See how many bytes we need to store the image  
+            int size = image.GetConvertedDataSize(conversionParams);
+
+            // Allocate a buffer to store the image.
+            var buffer = new NativeArray<byte>(size, Allocator.Temp);
+
+            //Extract the data from the image
+            image.Convert(conversionParams, buffer);
+
+            //Now we dispose the XRCpuImage because we have everything we need in the buffer.
+            image.Dispose();
+
+            //We now make the Texture2D that we need for the decode section.
+            _cameraImageTexture = new Texture2D(
+                conversionParams.outputDimensions.x,
+                conversionParams.outputDimensions.y,
+                conversionParams.outputFormat,
+                false);
+
+            _cameraImageTexture.LoadRawTextureData(buffer);
+            _cameraImageTexture.Apply();
+
+            buffer.Dispose();
+
+            _result = _barcodeReader.Decode(
+                _cameraImageTexture.GetPixels32(),
+                _cameraImageTexture.width,
+                _cameraImageTexture.height);
+
+            if (_result != null)
+            {
+                _lastResult = _result.Text + " " + _result.BarcodeFormat;
+                Debug.Log(_lastResult);
+            }
+
+
+        }
     }
 }
+
+
+
+
+
